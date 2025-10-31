b342f0fde49a3c6725ee9127ec0f3c8feaad3a1e
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-10-31 16:56:21 +0530

    ux fixes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { tweetsData } from "./data.js";
 import { v4 as uuidv4 } from "https://cdn.jsdelivr.net/npm/uuid@9.0.1/dist/esm-browser/index.js";
 
-const tweetInput = document.getElementById("tweet-input");
-
 
 document.addEventListener("click", function (e) {
     if (e.target.dataset.like) {
@@ -51,8 +49,11 @@ function handleReplyClick(replyId) {
     document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
 }
 
-function handleTweetBtnClick(e){
-        tweetsData.unshift({
+function handleTweetBtnClick(){
+    const tweetInput = document.getElementById("tweet-input");
+    
+    if(tweetInput.value) {
+         tweetsData.unshift({
              handle: `@Scrimba`,
              profilePic: `images/scrimbalogo.png`,
              likes: 0,
@@ -65,6 +66,7 @@ function handleTweetBtnClick(e){
         })
         render();
         tweetInput.value = ''
+     }
     }
 
 
