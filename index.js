e826fa73a156371fc2712081a0006aa078d6c5a7
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-10-31 16:18:16 +0530

    updating like comment retweet functionality

--- index.js
+++ index.js
@@ -1,16 +1,94 @@
 import { tweetsData } from "./data.js";
 
-const tweetBtn = document.getElementById("tweet-btn");
 const tweetInput = document.getElementById("tweet-input");
 
 
-tweetBtn.addEventListener("click", function () {
-    console.log(tweetInput.value);
+document.addEventListener("click", function (e) {
+    if (e.target.dataset.like) {
+        handleLikeClick(e.target.dataset.like)
+    } else if (e.target.dataset.retweet) {
+        handleRetweetClick(e.target.dataset.retweet)
+    } else if (e.target.dataset.reply) {
+        handleReplyClick(e.target.dataset.reply)
+    } else if (e.target.id === "tweet-btn") {
+        handleTweetBtnClick()
+    }
 })
+
+function handleLikeClick(tweetId) {
+    // So we can take the object at position zero. because it gives one object when we like
+    const targetTweetObj = tweetsData.filter((tweet) => {
+        return tweet.uuid === tweetId
+    })[0]
+    if (targetTweetObj.isLiked) {
+        targetTweetObj.likes--
+    }
+    else {
+        targetTweetObj.likes++
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    render();
+}
+
+function handleRetweetClick(tweetId) {
+    //only one object will be returned so we take the first object at position zero
+    const targetTweetObj = tweetsData.filter((tweet) => {
+        return tweet.uuid === tweetId
+    })[0]
+    if (targetTweetObj.isRetweeted) {
+        targetTweetObj.retweets--
+    }
+    else {
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render();
+}
+
+
+function handleReplyClick(replyId) {
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+}
+
+function handleTweetBtnClick(){
+    console.log(tweetInput.value)
+}
+
 
 function getFeedHtml() {
     let feedHtml = "";
     tweetsData.forEach((tweet) => {
+
+        let likeIconClass = ''
+        let retweetIconClass = ''
+
+        if (tweet.isLiked) {
+            likeIconClass = 'liked'
+        }
+
+        if (tweet.isRetweeted) {
+            retweetIconClass = 'retweeted'
+        }
+
+        let repliesHtml = ''
+
+        if (tweet.replies.length > 0) {
+            tweet.replies.forEach((reply) => {
+                repliesHtml += `
+                        <div class="tweet-reply">
+                           <div class="tweet-inner">
+                              <img src="${reply.profilePic}" class="profile-pic">
+                                  <div>
+                                     <p class="handle">${reply.handle}</p>
+                                     <p class="tweet-text">${reply.tweetText}</p>
+                                  </div>
+                           </div>
+                        </div>`
+            })
+        }
+
+
+
         feedHtml += `
           <div class="tweet">
            <div class="tweet-inner">
@@ -24,16 +102,20 @@ function getFeedHtml() {
                         ${tweet.replies.length}
                       </span>
                       <span class="tweet-detail">
-                      <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
+                      <i class="fa-solid fa-heart ${likeIconClass}" data-like="${tweet.uuid}"></i>
                         ${tweet.likes}
                       </span>
                       <span class="tweet-detail">
-                      <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
+                      <i class="fa-solid fa-retweet ${retweetIconClass}" data-retweet="${tweet.uuid}"></i>
                         ${tweet.retweets}
                       </span>
                   </div>   
               </div>            
            </div>
+           <div class="hidden" id="replies-${tweet.uuid}">
+                  <!-- REPLIES HERE -->
+                 ${repliesHtml}
+           </div>   
           </div>
         `
     })
